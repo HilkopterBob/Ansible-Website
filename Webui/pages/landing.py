75e0edc4e82b4b8a0a710293c24b113b8abe31c1
Nick <podewils08@gmail.com>
2023-03-28 11:44:18 +0200

    expanded right drawer content

--- Webui/pages/landing.py
+++ Webui/pages/landing.py
@@ -22,13 +22,25 @@ def landng_page(request: Request) -> None:
     if not is_authenticated(request):
         return RedirectResponse('/login')
     session = session_info[request.session['id']]
-    with ui.header().style("background-color: #1d1d1d").classes("flex flex-wrap items-center justify-between w-full justify-self-center"):
-        with ui.row().classes("w-full p-0"):
-            with ui.row().classes('flex items-center self-center'):
-                ui.label("büttnerlogo")
-            ui.label("").classes("w-10/12")
-            with ui.row().classes('grid justify-self-end'):
-                #hier buttons für andere pages einfügen
-                ui.button("", on_click=lambda: print("hallo") ).props("icon=person")
-        
+
+
+    # ================ Landing Page ================ #
+    with ui.header().style("background-color: #1d1d1d").classes("items-center justify-between"):
+                ui.image("http://127.0.0.1:3000/buettner-nice-removebg-preview.png").style("width:150px; height:50px")
+                ui.label("Ansible Automation Hub").style("color:linear-gradient(to right,red,yellow)").classes("text-h6")
+                with ui.row():
+                    ui.button(on_click=lambda: right_drawer.toggle()).props("icon=menu outline")
+
+    with ui.right_drawer(value=False, fixed=False).style('background-color: #1d1d1d').props('bordered') as right_drawer:
+        with ui.expansion("Account", icon="person"):
+            ui.label(" ")
+            ui.button("settings", on_click=lambda: ui.open("/settings")).classes("w-full").props("align=left outline icon=logout")
+            ui.button("log out", on_click=lambda: ui.open("/logout")).classes("w-full").props("align=left outline icon=logout")
+        with ui.expansion("Docs", icon="person"):
+            ui.label(" ")
+            ui.button("Open Docs", on_click=lambda: ui.open("/settings")).classes("w-full").props("align=left outline icon=logout")
+            with ui.expansion("Paragraphs", icon="person"):
+                ui.label(" ")
+                ui.button("settings", on_click=lambda: ui.open("/settings")).classes("w-full").props("align=left outline icon=logout")
+                ui.button("log out", on_click=lambda: ui.open("/logout")).classes("w-full").props("align=left outline icon=logout")
     ui.label("hi")
